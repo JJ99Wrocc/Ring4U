d455237f7f1ca7e88034380015bf6f48fb5df755
Iwona Esangbedo <esangbedojoachim@gmail.com>
2026-05-19 20:56:09 +0200

    Updates

--- src/utils/loadXML.js
+++
@@ -1,8 +0,0 @@
-import { parseStringPromise } from "xml2js";
-
-export async function loadXML(url) {
-  const response = await fetch(url);
-  const text = await response.text();
-  const data = await parseStringPromise(text, { explicitArray: false });
-  return data;
-}
